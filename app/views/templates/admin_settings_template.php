6ba718f0128b528c2cb1023b210da9916b0fe196
Jan Capek <jan.capek0@gmail.com>
2018-10-12 18:15:07 +0200

    UI completely updated

--- app/views/templates/admin_settings_template.php
+++ app/views/templates/admin_settings_template.php
@@ -1,11 +1,11 @@
-<div class="container">
+<div id="admin-settings" class="container">
 <h3>Postavke</h3>
     <?php foreach($messages as $message): ?>
         <a class="green-text"><?= safe($message); ?></a><br>
     <?php endforeach; ?>
 
     <div class="row">
-        <ul class="collection with-header" style="padding-bottom: 1.5em;">
+        <ul class="collection with-header">
             <li class="collection-header"><h5 class="subheading-text">Postavke korisničkog imena</h5></li>
             
             <li class="collection-item">
@@ -14,7 +14,7 @@
                     <a class="red-text"><?= safe($error); ?></a><br>
                 <?php endforeach; ?>
 
-                <form id="update_username" class="col s12 white" method="post" action="?controller=settings&update=username">
+                <form id="update_username" class="col s12" method="post" action="?controller=settings&update=username">
                     <!-- username -->
                     <div class="row">
                         <div class="input-field col l6 s12">
@@ -40,7 +40,7 @@
 
     <div class="row">
 
-        <ul class="collection with-header" style="padding-bottom: 1.5em;">
+        <ul class="collection with-header">
             <li class="collection-header"><h5 class="subheading-text">Promjena lozinke</h5></li>
             
             <li class="collection-item">
@@ -49,7 +49,7 @@
                     <a class="red-text"><?= safe($error); ?></a><br>
                 <?php endforeach; ?>
                 
-                <form id="update_password" class="col s12 white" method="post" action="?controller=settings&update=password">
+                <form id="update_password" class="col s12" method="post" action="?controller=settings&update=password">
                     <!-- password -->
                     <div class="row">
                         <div class="input-field col l6 s12">
--- public/basicScript.js
+++ public/basicScript.js
@@ -1,19 +1,4 @@
 $(document).ready(() => {
-
-    /* // pick init
-    $('select').material_select();
-
-    //hamburger button for navbar
-    $(".button-collapse").sideNav({
-        draggable: true
-    });
-    $(".dropdown-button").dropdown();
-    $('.slider').slider({
-        indicators: false,
-        height: 500
-    });
-    $('.modal').modal(); */
-
     $('.tabs').tabs();
     $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
